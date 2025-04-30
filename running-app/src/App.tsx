0670ec5a9c2935beb5aecfd8a6efa3282495c1e4
IsaacYu <yuisaac@outlook.com>
2025-04-30 18:27:39 -0400

    init running app

---
+++ running-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Geolocate from "./GeoLocate";
+
+function App() {
+
+  return (
+    <div>
+      <div>Hello</div>
+      <Geolocate/>
+    </div>
+  );
+}
+
+export default App;
---
+++ running-app/src/Geolocate.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { LatLongCoordinates } from "./Types";
+
+const Geolocate: React.FC = () => {
+  const [path, setPath] = useState<LatLongCoordinates[]>();
+
+  function updateGeolocation(position: GeolocationPosition) {
+    setPath((prevPath) => [
+      ...(prevPath ?? []),
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+    ]);
+  }
+
+  function displayError() {
+    alert("Position is not available!");
+  }
+
+  useEffect(() => {
+    const locationInterval = setInterval(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          updateGeolocation,
+          displayError
+        );
+      }
+    }, 2500);
+
+    return () => clearInterval(locationInterval);
+  }, []);
+
+  useEffect(() => {
+    console.log(path);
+  }, [path]);
+
+  return (
+    <div>
+      {path?.map((coordinate) => (
+        <div>
+          <h3>{coordinate.latitude} {coordinate.longitude}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Geolocate;
